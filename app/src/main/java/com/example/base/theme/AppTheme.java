5f52cd591245e6ca7dd6b6b150bf39dd91271240
arun129lokith <93975592+arun129lokith@users.noreply.github.com>
2023-10-10 18:11:08 +0530

    Colors and Fonts

---
+++ app/src/main/java/com/example/base/theme/AppTheme.java
@@ -0,0 +1,32 @@
+package com.example.base.theme;
+
+public class AppTheme {
+
+    public static final int LIGHT_THEME_BACKGROUND = R.color.light_theme_background;
+    public static final int Dark_THEME_BACKGROUND = R.color.dark_theme_background;
+    public static final int LIGHT_THEME_FONT_COLOR = R.color.normal_font;
+    public static final int DARK_THEME_FONT_COLOR = R.color.dark_theme_font;
+
+    public static final int DEFAULT_FONT = R.font.lexend;
+
+    public static final int TITLE_FONT_COLOR = R.color.title_font;
+    public static final int SUBTITLE_FONT_COLOR = R.color.sub_title_font;
+    public static final int DEFAULT_FONT_COLOR = R.color.normal_font;
+
+    public static final int TITLE_FONT_SIZE = 16;
+    public static final int DEFAULT_FONT_SIZE = 14;
+
+    public static final int TITLE_FONT_WEIGHT = 600;
+    public static final int SUBTITLE_FONT_WEIGHT = 500;
+    public static final int NORMAL_FONT_WEIGHT = 400;
+
+    public static final int ERROR_COLOR = R.color.error_color;
+    public static final int HEADER_OUTLINE_COLOR = R.color.header_outline_color;
+    public static final int COMPONENT_BACKGROUND_COLOR = R.color.component_background;
+    public static final int BUTTON_STATE_TEXT_COLOR = R.color.component_state_text_color;
+
+    public static final int APP_PRIMARY_COLOR = R.color.app_primary;
+    public static final int APP_SECONDARY_COLOR = R.color.app_secondary;
+    public static final int APP_ACCENT_COLOR = R.color.app_accent;
+
+}
